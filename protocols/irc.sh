0fc6131bab2b797ab5b9c0f05ebd8744cde94979
Alicia <alicia@ion.nu>
2017-03-28 15:57:36 +0200

    Added license notices.

--- protocols/irc.sh
+++ protocols/irc.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+#
+#   relaybot, the message-passing bot that grew into so much more
+#   Copyright (C) 2015-2017  alicia@ion.nu
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, version 3 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 if [ -z "$port" ]; then port=6667; fi
 protocol_cmd="`which nc` '${host}' '${port}'"
 
